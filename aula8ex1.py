7ad38b56b2be44597137574524dfc32e8105faea
gabriela-marinho <g.barreto.marinho@gmail.com>
2021-05-19 14:08:43 -0300

    atualizado falta ex 2

--- aula8ex1.py
+++ aula8ex1.py
@@ -1,3 +1,13 @@
+# 1.	Dada a lista L = [5, 7, 2, 9, 4, 1, 3], escreva um programa que imprima as seguintes informações:
+# a.	tamanho da lista.
+# b.	maior valor da lista.
+# c.	menor valor da lista.
+# d.	soma de todos os elementos da lista.
+# e.	lista em ordem crescente.
+# f.	lista em ordem decrescente.
+
+
+
 list =  [5, 7, 2, 9, 4, 1, 3]
 resultado = len(list)
 print(resultado)
