ed5e20257da1ab3138a92720c9f959626b90aba0
gabriela-marinho <g.barreto.marinho@gmail.com>
2021-05-18 22:55:19 -0300

    .

---
+++ aula8ex1.py
@@ -0,0 +1,18 @@
+list =  [5, 7, 2, 9, 4, 1, 3]
+resultado = len(list)
+print(resultado)
+
+resultado = max(list)
+print(resultado)
+
+resultado = min(list)
+print(resultado)
+
+resultado = sum(list)
+print(resultado)
+
+list.sort()
+print(list)
+
+list.reverse()
+print(list)
