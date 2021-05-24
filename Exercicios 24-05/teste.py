265bac0659074888f1ec9fd2e863e6276bb5599b
gabriela-marinho <g.barreto.marinho@gmail.com>
2021-05-24 09:38:46 -0300

    arquivo para testar apenas

--- Exercicios 24-05/teste.py
+++ Exercicios 24-05/teste.py
@@ -1,18 +1,3 @@
 
 
 
-vogais = ['a', 'e', 'i', 'o', 'u']
-
-palavra = input('Digite a palavra:\t')
-
-resultado = []
-
-for letra in palavra:
-
-    for vogal in vogais:
-
-        if letra == vogal:
-
-            resultado.append(vogais)
-
-print(f'As vogais são: { resultado }')
