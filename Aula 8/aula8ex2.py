a89db167aafffda7f96c2af52954417ab1849403
gabriela-marinho <g.barreto.marinho@gmail.com>
2021-05-24 04:23:49 -0300

    Feito exercicio2 até while, mas ainda nao consegui reunir a lista com o for para as tentativas

--- Aula 8/aula8ex2.py
+++ Aula 8/aula8ex2.py
@@ -3,20 +3,46 @@
 
 import random
 
-palavras_forca = ['programa','python','bug','sistema']
+palavras_forca = [['s','i','m']]
 tentativas_lista = []
-# tentativasTotais = 6
+tentativasTotais = 0
 palavra_escolhida = []
 palavra = random.choice(palavras_forca)
 
 
 
-print("A palavra escolhida pelo sistema tem", len(palavra)  , "letras, e você tem 6 tentativas antes de ser enforcado")
+print("A palavra escolhida pelo sistema tem", len(palavra)  ,"letras, e você tem 6 tentativas antes de ser enforcado")
 palavra_escolhida.append(palavra)
 print(palavra_escolhida)
 
-for letra in palavra_escolhida:
-    count(palavra_escolhida)
+while tentativasTotais < 6:
+    tentativasTotais+= 1
+    tentativa = tentativas_lista.append((input("Digite uma letra \n")))
+    print(f"Vc usou a {tentativasTotais}ª tentativas")
+    print(f'Letras utilizadas{tentativas_lista}')
+    if palavra_escolhida
+        #tentativas_lista = tentativa
+        for item in palavra_escolhida:
+             print(item)
+        
+    
+    # if palavra_escolhida in tentativas_lista : # nao esta dando certo pois o conjunto lê todas as variaveis do conjunto, logo as certas e erradas
+    #     for item  in tentativas_lista:
+    #         print(tentativas_lista in item , f"Acertou a palavra toda")
+    
+    # if tentativasTotais == 6:
+    #     print("Vc foi enforcado!!!")
+    
+# for letra in palavra_escolhida:
+#     print(letra)
+    # if letra == tentativas_lista:
+    #         print("Acertou uma letra")
+
+# for letra in palavra_escolhida:
+#     if letra in tentativas_lista:
+#         print ("Vc acertou uma letra")
+
+
 
 # while True:
 #     tentativa = tentativas_lista.append (input("Digite uma letra ou 'sair' para fechar o programa\n"))
