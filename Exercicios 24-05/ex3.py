1bf38e56c12f34cb276df8d4cc4d0473df7dfb24
gabriela-marinho <g.barreto.marinho@gmail.com>
2021-05-24 14:07:14 -0300

    falta exercicios 3 e 4 para finalizar

--- Exercicios 24-05/ex3.py
+++ Exercicios 24-05/ex3.py
@@ -1,9 +1,12 @@
-#03 - Utilizando estruturas de repetição com teste lógico, faça um programa que peça uma senha para iniciar seu 
+# 03 - Utilizando estruturas de repetição com teste lógico, faça um programa que peça uma senha para iniciar seu 
 # processamento, só deixe o usuário continuar se a senha estiver correta, após entrar dê boas vindas a seu usuário e 
 # apresente a ele o jogo da advinhação, onde o computador vai “pensar” em um número inteiro entre 0 e 20. 
 # O jogador vai tentar adivinhar qual número foi escolhido até acertar, a cada palpite do usuário diga a ele se o número 
 # escolhido pelo computador é maior ou menor ao que ele palpitou, no final mostre quantos palpites foram necessários 
 # para vencer.
+
+
+import random
 
 senha = input("Cadastre sua senha:")
 print("Senha cadastrada com sucesso!!!")
@@ -12,14 +15,36 @@ print()
 print("                                                    SISTEMA DE VALIDAÇÃO                                       ")
 print(" Para entrar no sistema digite:")
 print()
-digitoSenha = input("Senha:")
+usuario = input("Usuario:")
 
-# while senha != digitoSenha:
-#     print("***SENHA INCORRETA***")
-#     break
+
+numeros = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+escolhido = random.choice(numeros)
+numeroEscolhido = []
+digitoSenha = input("Senha:")
+print(numeroEscolhido)
+
     
-while True:
-    senha != digitoSenha
-    print("***SENHA INCORRETA***")
-    digitoSenha = input("Senha:")
-break    
+
+while senha != digitoSenha:
+
+        print("***SENHA INCORRETA***")
+        
+if senha == digitoSenha:
+    print("Senha correta!")
+    print()
+    print(f"Bem -Vindo(a), {usuario}! ")
+    print()
+    print("O Sistema pensará em um numero de 0 a 20. Tente adivinhar!")
+    print()
+    palpite = input("Tente acertar o número, qual seu palpite?\n")
+    if palpite != numeroEscolhido:
+        print("Não foi dessa vez")
+    elif palpite == numeroEscolhido:
+        print("Vc acertou o numero, parabéns!!!") 
+    elif palpite > 10:
+        print(" O número é maior que 10")
+    elif palpite < 10:
+        print(" O número é menor que 10")
+    
+                   
--- Exercicios 24-05/ex4.py
+++ Exercicios 24-05/ex4.py
@@ -1,2 +1,16 @@
 #04 - Utilizando funções e listas faça um programa que receba uma data no formato DD/MM/AAAA e devolva uma string no 
 # formato D de mesPorExtenso de AAAA. Valide a data e retorne NULL caso a data seja inválida.
+
+
+def extenso_mes(mes):
+	lista_de_mes = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+	mes_ex = lista_de_mes[mes-1]
+	return mes_ex
+
+def extenso_dia(dia):
+
+	lista_de_dia = ["Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Onze", "Doze", "Treze", "Catorzer", "Quinze", "Dezesseis", "Dezessete", "Dezoito", "Dezenove", "Vinte", "Vinte e Um", "Vinte e Dois", "Vinte e Três", "Vinte e Quatro", "Vinte e Cinco", "Vinte e Seis", "Vinte e Sete", "Vinte e Oito", "Vinte e Nove", "Trinta", "Trinta e Um"]
+	dia_ex = lista_de_dia[dia-1]
+	return dia_ex
+
+
